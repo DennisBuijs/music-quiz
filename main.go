305309d53c674b979a2ed0f00e90501d47f988a4
Dennis Buijs <dennisdennisbuijs@gmail.com>
2024-10-28 22:04:41 +0100

    use gezellige tracks

--- main.go
+++ main.go
@@ -28,21 +28,21 @@ func main() {
 	var songs []Song
 
 	songs = append(songs, Song{
-		"Piet Friet",
-		"Broodje Frik",
-		"https://p.scdn.co/mp3-preview/a4681d2ecf1a594e5ac3170379eaa5de85a3b017?cid=cfe923b2d660439caf2b557b21f31221",
+		"Guus Meeuwis",
+		"Geef Mij Je Angst",
+		"https://p.scdn.co/mp3-preview/0f8a68d9ca5fef269fe77cefd0087f0a6c390d10?cid=cfe923b2d660439caf2b557b21f31221",
 	})
 
 	songs = append(songs, Song{
-		"Aart Appeltaart",
-		"Suiker in de Thee",
-		"https://p.scdn.co/mp3-preview/f15fb7b8651c5988c4e36bc4ccc82664cea64b2a?cid=cfe923b2d660439caf2b557b21f31221",
+		"Guus Meeuwis",
+		"Brabant",
+		"https://p.scdn.co/mp3-preview/d23338bd240c9cb8cc99f8c0f28eb6f4214b9e5b?cid=cfe923b2d660439caf2b557b21f31221",
 	})
 
 	songs = append(songs, Song{
-		"Fred Kroket",
-		"Tien Uur Snacks",
-		"https://p.scdn.co/mp3-preview/7f75e97880ceef1e88fc8097a568d765bc1f555c?cid=cfe923b2d660439caf2b557b21f31221",
+		"Guus Meeuwig",
+		"Per Spoor (Kedeng Kedeng)",
+		"https://p.scdn.co/mp3-preview/a17e3e78be00473fe527e99cc1246e2244fa43cb?cid=cfe923b2d660439caf2b557b21f31221",
 	})
 
 	lobby := &Lobby{
